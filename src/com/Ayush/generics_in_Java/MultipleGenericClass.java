cea79ba7be83cb5f3fea5901e9834b67737f98a5
Ayush khandekar <ayushkhandekar33@gmail.com>
2026-01-20 13:29:56 +0530

    Multiple Generic Example

---
+++ src/com/Ayush/generics_in_Java/MultipleGenericClass.java
@@ -0,0 +1,22 @@
+package com.Ayush.generics_in_Java;
+
+public class MultipleGenericClass <T,U>{
+    T obj1;
+    U obj2;
+    MultipleGenericClass(T obj1, U obj2){
+        this.obj1=obj1;
+        this.obj2=obj2;
+
+    }
+    public void print(){
+        System.out.println(obj1);
+        System.out.println(obj2);
+    }
+    public static void main(String[] args) {
+       MultipleGenericClass<Integer,String> myObj1 = new MultipleGenericClass<Integer, String>(1,"Ayush");
+       MultipleGenericClass<String ,String > myObj2 = new MultipleGenericClass<String, String>("1","Arman");
+       myObj1.print();
+       myObj2.print();
+
+    }
+}
