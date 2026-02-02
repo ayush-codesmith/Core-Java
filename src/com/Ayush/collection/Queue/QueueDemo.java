dd9a0dbb316683e2178934a94f69b34a0d9de849
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-02 22:50:52 +0530

    Queue Interface Started

--- src/com/Ayush/collection/Queue/QueueDemo.java
+++ src/com/Ayush/collection/Queue/QueueDemo.java
@@ -1,4 +1,27 @@
 package com.Ayush.collection.Queue;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Person 1");
+        queue.add("Person 2");
+        queue.add("Person 3");
+        queue.add("Person 4");
+        queue.add("Person 5");
+        queue.add("Person 6");
+        queue.add("Person 7");
+        queue.add("Person 8");
+        Iterator<String> iterator = queue.iterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next()+"-- ");
+        }
+        System.out.println();
+        String remove = queue.remove();
+        System.out.println(queue);
+
+    }
 }
