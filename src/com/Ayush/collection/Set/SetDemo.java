05b809580a2b6e0052152bde1fd8fa2f8c4ff86c
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-02 22:05:05 +0530

    LinkedHashSet Done

--- src/com/Ayush/collection/Set/SetDemo.java
+++ src/com/Ayush/collection/Set/SetDemo.java
@@ -2,21 +2,33 @@ package com.Ayush.collection.Set;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetDemo {
     public static void main(String[] args) {
         // Only 1 point is main set does not contain duplicates
-        Set<Integer> set = new HashSet<>();
-        set.add(1);
-        set.add(2);
-        set.add(3);
-        set.add(4);
+//        Set<Integer> set = new HashSet<>();
+//        set.add(1);
+//        set.add(2);
+//        set.add(3);
+//        set.add(4);
+//
+//        Iterator<Integer> iterator = set.iterator();
+//        while (iterator.hasNext()){
+//            System.out.print(iterator.next()+" ");
+//        }
 
-        Iterator<Integer> iterator = set.iterator();
-        while (iterator.hasNext()){
-            System.out.print(iterator.next()+" ");
-        }
+        // In LinkedHashSet it will maintain insertions order
+        Set<Integer> set = new LinkedHashSet<>();
+        set.add(2);
+        set.add(6);
+        set.add(9);
+        set.add(1);
+        System.out.println(set);
+
+
+
 
 
 
