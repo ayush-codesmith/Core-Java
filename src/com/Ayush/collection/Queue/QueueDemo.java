403e9bb22946446f1c48214ff32abd9dc66bb0a7
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-02 22:54:51 +0530

    PriorityQueue Done

--- src/com/Ayush/collection/Queue/QueueDemo.java
+++ src/com/Ayush/collection/Queue/QueueDemo.java
@@ -2,11 +2,29 @@ package com.Ayush.collection.Queue;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class QueueDemo {
     public static void main(String[] args) {
-        Queue<String> queue = new LinkedList<>();
+        // we can use LinkedList or there are other also Classes
+//        Queue<String> queue = new LinkedList<>();
+//        queue.add("Person 1");
+//        queue.add("Person 2");
+//        queue.add("Person 3");
+//        queue.add("Person 4");
+//        queue.add("Person 5");
+//        queue.add("Person 6");
+//        queue.add("Person 7");
+//        queue.add("Person 8");
+//        Iterator<String> iterator = queue.iterator();
+//        while (iterator.hasNext()){
+//            System.out.print(iterator.next()+"-- ");
+//        }
+//        System.out.println();
+//        String remove = queue.remove();
+//        System.out.println(queue);
+        Queue<String > queue = new PriorityQueue<>();
         queue.add("Person 1");
         queue.add("Person 2");
         queue.add("Person 3");
@@ -14,14 +32,9 @@ public class QueueDemo {
         queue.add("Person 5");
         queue.add("Person 6");
         queue.add("Person 7");
-        queue.add("Person 8");
         Iterator<String> iterator = queue.iterator();
         while (iterator.hasNext()){
-            System.out.print(iterator.next()+"-- ");
+            System.out.println(iterator.next());
         }
-        System.out.println();
-        String remove = queue.remove();
-        System.out.println(queue);
-
     }
 }
