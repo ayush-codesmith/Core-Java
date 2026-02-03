b73c1ba6db98e3a20ab7d567601e9fc91abf5b5c
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-03 13:55:48 +0530

    3 ways of Deque

--- src/com/Ayush/collection/Queue/DqueueDemo.java
+++ src/com/Ayush/collection/Queue/DqueueDemo.java
@@ -1,4 +1,17 @@
 package com.Ayush.collection.Queue;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
 public class DqueueDemo {
+    // it is a duoble ended queue
+//    Deque<Integer> deque1 = new LinkedList<>();
+//    Deque<Integer> deque2 = new ArrayDeque<>();
+//    Deque<Integer> deque3 = new ConcurrentLinkedDeque<>();
+//    // this are 3 classe to implemetn Deque
+
+
+
 }
