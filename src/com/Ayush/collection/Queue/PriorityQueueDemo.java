25a1ac17e1331d4ed905d7a29d697e509aacf799
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-03 12:52:02 +0530

    PriorityQueue Done

--- src/com/Ayush/collection/Queue/PriorityQueueDemo.java
+++ src/com/Ayush/collection/Queue/PriorityQueueDemo.java
@@ -1,15 +1,35 @@
 package com.Ayush.collection.Queue;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQueueDemo {
     public static void main(String[] args) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        queue.add(50);
-        queue.add(3);
-        queue.add(1);
-        queue.add(7);
-        queue.add(9);
-        System.out.println(queue.peek());
+//        PriorityQueue<Integer> queue = new PriorityQueue<>();
+//        queue.add(50);
+//        queue.add(3);
+//        queue.add(1);
+//        queue.add(7);
+//        queue.add(9);
+//        System.out.println(queue.peek());
+
+        PriorityQueue<String > queue = new PriorityQueue<>();
+        PriorityQueue<String > queue1 = new PriorityQueue<>(Comparator.reverseOrder());
+        // this is for integer ... for string there is a other way to write lambda expressions
+//        PriorityQueue<Integer > queue2 = new PriorityQueue<>((x,y) -> {
+//            return x - y;
+//        });
+        queue.add("A");
+        queue.add("U");
+        queue.add("C");
+        queue.add("F");
+        queue.add("B");
+        while (!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+        // we can use in constructor
+        // Comparator.reverseOrder();
+        // We can use lambda  Function
+
     }
 }
