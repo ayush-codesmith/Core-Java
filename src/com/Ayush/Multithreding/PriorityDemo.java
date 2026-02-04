ecfec12f40905248a767c402086f3c5d645b2901
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-04 12:34:09 +0530

    Interrupt Method

--- src/com/Ayush/Multithreding/PriorityDemo.java
+++ src/com/Ayush/Multithreding/PriorityDemo.java
@@ -6,17 +6,31 @@ public class PriorityDemo extends Thread {
    public PriorityDemo(String name){
        super(name);
    }
+//
+//    @Override
+//    public void run() {
+
+
+
+//       for (int i=0;i<5;i++){
+//           try {
+//               Thread.sleep(1000);
+//           } catch (InterruptedException e) {
+//               e.printStackTrace();
+//           }
+//           System.out.println(Thread.currentThread().getName()+"---"+Thread.currentThread().getPriority());
+//       }
+//    }
+
 
     @Override
     public void run() {
-       for (int i=0;i<5;i++){
-           try {
-               Thread.sleep(1000);
-           } catch (InterruptedException e) {
-               e.printStackTrace();
-           }
-           System.out.println(Thread.currentThread().getName()+"---"+Thread.currentThread().getPriority());
-       }
+        try {
+            Thread.sleep(1000);
+            System.out.println("My Name Is Ayush");
+        } catch (InterruptedException e) {
+            System.out.println("Exception "+e);
+        }
     }
 
     public static void main(String[] args) {
@@ -24,6 +38,7 @@ public class PriorityDemo extends Thread {
         PriorityDemo p1 = new PriorityDemo("Ayush");
         p1.setPriority(Thread.MAX_PRIORITY);
         p1.start();
+        p1.interrupt();
 
 
     }
