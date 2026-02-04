8bc1a88e7a3c85a0f81d067982767826e0a6effe
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-04 12:27:31 +0530

    Undersatand Priority Method

--- src/com/Ayush/Multithreding/PriorityDemo.java
+++ src/com/Ayush/Multithreding/PriorityDemo.java
@@ -1,7 +1,25 @@
 package com.Ayush.Multithreding;
 
-public class PriorityDemo {
+public class PriorityDemo extends Thread {
+
+
+    @Override
+    public void run() {
+       for (int i=0;i<5;i++){
+           try {
+               Thread.sleep(1000);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+           System.out.println(Thread.currentThread().getName()+"---"+Thread.currentThread().getPriority());
+       }
+    }
+
     public static void main(String[] args) {
+
+        PriorityDemo p1 = new PriorityDemo();
+        p1.start();
+
 
     }
 }
