660be82bfb47a6903887e51297e907bbf3bd4c1f
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-05 19:39:58 +0530

    File Modified

--- src/com/Ayush/Multithreding/CounterDemo.java
+++ src/com/Ayush/Multithreding/CounterDemo.java
@@ -1,7 +1,17 @@
 package com.Ayush.Multithreding;
 
 public class CounterDemo {
+    private int c =0;
+
+    public void inc(){
+        c++;
+    }
+    public int getC(){
+        return c;
+    }
     public static void main(String[] args) {
+
+
 
     }
 }
