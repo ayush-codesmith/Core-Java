daf1dbfa7f181ee50f95bd4dbd310bee651d6fb2
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-05 20:10:01 +0530

    Added one sec sleep to understand in detail

--- src/com/Ayush/Multithreding/StaticDemo.java
+++ src/com/Ayush/Multithreding/StaticDemo.java
@@ -3,6 +3,12 @@ class Table{
     synchronized static void printTable(int n){
         for (int i=1;i<=10;i++){
             System.out.println(n*i);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         System.out.println();
     }
