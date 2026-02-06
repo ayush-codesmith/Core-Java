31d75d0c13d41a99e10856039e12be20a9d0a032
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-06 12:47:26 +0530

    Write logic in ObjectL file

--- src/com/Ayush/Multithreding/ObjecL.java
+++ src/com/Ayush/Multithreding/ObjecL.java
@@ -1,6 +1,13 @@
 package com.Ayush.Multithreding;
 
 public class ObjecL {
-
+public synchronized void display(){
+    System.out.println(Thread.currentThread().getName()+" acquired object-level lock.");
+    try {
+        Thread.sleep(1000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+}
 
 }
