2ebd86b1a527555bce69589433fc658a766e48c7
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-06 20:06:38 +0530

    add ReentrantLock and multithreading logic to ExplictLockE1

--- src/com/Ayush/Multithreding/ExplictLockE1.java
+++ src/com/Ayush/Multithreding/ExplictLockE1.java
@@ -1,16 +1,57 @@
 package com.Ayush.Multithreding;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 class Ex{
 
     private int balance = 1000;
+    private final Lock lock = new ReentrantLock();
 
     public void withdraw(int amount){
+       try {
+           System.out.println(Thread.currentThread().getName()+" Attempting to withdraw");
+           if(lock.tryLock()){
+               if (balance>=amount){
+                   try {
+                   System.out.println(Thread.currentThread().getName()+" Operation ongoing withdraw "+amount);
+                   balance-=amount;
+                   Thread.sleep(3000);
+                   System.out.println("Balance is now "+balance+" After withdraw "+amount);
+                   }catch (Exception e){
+                       Thread.currentThread().interrupt();
+                   }finally {
+                       lock.unlock();
+                   }
+
+               }else {
+                   System.out.println("Insufficient Balance !!!!");
+               }
+
+           }else {
+               System.out.println("Operation is Lock we will try again !!");
+           }
+
+       }catch (Exception e){
+          Thread.currentThread().interrupt();
+       }
 
     }
 
 }
 public class ExplictLockE1 {
     public static void main(String[] args) {
+        Ex op = new Ex();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                op.withdraw(100);
+            }
+        };
+        Thread t1 = new Thread(runnable,"Thread-1");
+        Thread t2 = new Thread(runnable,"Thread-2");
+        t1.start();
+        t2.start();
 
     }
 
