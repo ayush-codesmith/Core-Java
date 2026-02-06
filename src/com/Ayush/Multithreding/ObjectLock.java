2b9c467c2a1779ed9784c6a228a7b1625a23b712
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-06 12:51:56 +0530

    Object-level lock Done

--- src/com/Ayush/Multithreding/ObjectLock.java
+++ src/com/Ayush/Multithreding/ObjectLock.java
@@ -2,6 +2,11 @@ package com.Ayush.Multithreding;
 
 public class ObjectLock {
     public static void main(String[] args) {
+    ObjecL l = new ObjecL();
+    Thread t1 = new Thread(l::display,"Thread-1");
+    Thread t2 = new Thread(l::display,"Thread-2");
+    t1.start();
+    t2.start();
 
     }
 }
