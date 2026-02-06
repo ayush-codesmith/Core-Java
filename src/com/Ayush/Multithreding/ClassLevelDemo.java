e5a8c28e572a47efabd2161beab82fe13b328c9e
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-06 12:57:57 +0530

    Class level Lock Done

--- src/com/Ayush/Multithreding/ClassLevelDemo.java
+++ src/com/Ayush/Multithreding/ClassLevelDemo.java
@@ -1,7 +1,21 @@
 package com.Ayush.Multithreding;
-
+class Clevel{
+    public  static synchronized void display(){
+        System.out.println(Thread.currentThread().getName()+" acquired object-level lock.");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
 public class ClassLevelDemo {
     public static void main(String[] args) {
+
+        Thread t1 = new Thread(Clevel::display,"Thread-1");
+        Thread t2 = new Thread(Clevel::display,"Thread-2");
+        t1.start();
+        t2.start();
 
     }
 }
