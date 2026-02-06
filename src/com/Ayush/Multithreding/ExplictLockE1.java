15d396622d03d5ffba9689c3a27855fb01ba2e83
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-06 19:53:43 +0530

    Second Try

---
+++ src/com/Ayush/Multithreding/ExplictLockE1.java
@@ -0,0 +1,17 @@
+package com.Ayush.Multithreding;
+
+class Ex{
+
+    private int balance = 1000;
+
+    public void withdraw(int amount){
+
+    }
+
+}
+public class ExplictLockE1 {
+    public static void main(String[] args) {
+
+    }
+
+}
