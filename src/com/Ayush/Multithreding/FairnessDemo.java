6d3f7a6e565d75c07308cd9ac8cec08d56e81ed1
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-09 14:35:18 +0530

    Enable fair ReentrantLock and enforce thread execution order

--- src/com/Ayush/Multithreding/FairnessDemo.java
+++ src/com/Ayush/Multithreding/FairnessDemo.java
@@ -4,16 +4,22 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 class Iml{
-    private final Lock lock= new ReentrantLock();
+    private final Lock lock= new ReentrantLock(true);
 
 
     public void method(){
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         lock.lock();
         try {
-            System.out.println(Thread.currentThread().getName()+" Done.");
+            System.out.println(Thread.currentThread().getName()+" Running..");
         }catch (Exception e){
             e.printStackTrace();
         }finally {
+            System.out.println(Thread.currentThread().getName()+" Done!!!");
             lock.unlock();
         }
 
@@ -22,14 +28,25 @@ class Iml{
 public class FairnessDemo {
     public static void main(String[] args) {
         Iml obj = new Iml();
+
+
+
         Thread t1 = new Thread(obj::method,"Thread-1");
         Thread t2 = new Thread(obj::method,"Thread-2");
         Thread t3 = new Thread(obj::method,"Thread-3");
         Thread t4 = new Thread(obj::method,"Thread-4");
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
+        try {
+
+            t1.start();
+            Thread.sleep(500);
+            t2.start();
+            Thread.sleep(500);
+            t3.start();
+            Thread.sleep(500);
+            t4.start();
+        }catch (Exception e){
+
+        }
 
 
     }
