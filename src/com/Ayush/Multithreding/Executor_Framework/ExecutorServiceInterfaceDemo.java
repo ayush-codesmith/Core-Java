8e31c9ce01301e03bf03672d2b8b3f0fda27746b
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-09 19:21:54 +0530

    Use fixed thread pool ExecutorService and submit tasks for execution timing

--- src/com/Ayush/Multithreding/Executor_Framework/ExecutorServiceInterfaceDemo.java
+++ src/com/Ayush/Multithreding/Executor_Framework/ExecutorServiceInterfaceDemo.java
@@ -5,19 +5,21 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecutorServiceInterfaceDemo {
+    private static Object TimeUnit;
+
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         ExecutorService service = Executors.newFixedThreadPool(3);
         for (int i=0;i<10;i++){
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
             service.submit(()->{
                 System.out.println("Running...");
             });
+
         }
         service.shutdown();
+
+        System.out.println(System.currentTimeMillis()-startTime);
 
     }
 }
