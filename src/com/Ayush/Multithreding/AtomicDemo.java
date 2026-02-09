04759869157b37a80149e0e207b3fbcb435eb370
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-09 13:45:06 +0530

    Implement AtomicDemo using AtomicInteger counter

--- src/com/Ayush/Multithreding/AtomicDemo.java
+++ src/com/Ayush/Multithreding/AtomicDemo.java
@@ -2,9 +2,39 @@ package com.Ayush.Multithreding;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+class Counter{
+    AtomicInteger count = new AtomicInteger();
 
+    public void method(){
+        count.incrementAndGet();
+    }
+}
 
 public class AtomicDemo {
+    public static void main(String[] args) throws InterruptedException {
 
 
+        Counter c = new Counter();
+
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 1; i <= 2000; i++) {
+                    c.method();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 1; i <= 2000; i++) {
+                c.method();
+            }
+        });
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        System.out.println(c.count);
+
+    }
 }
