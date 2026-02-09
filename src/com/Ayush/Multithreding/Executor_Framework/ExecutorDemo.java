8076448f29f386f3dc6f83bf8d47c316a506b493
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-09 18:35:37 +0530

    Implement basic Executor usage with lambda-based tasks

--- src/com/Ayush/Multithreding/Executor_Framework/ExecutorDemo.java
+++ src/com/Ayush/Multithreding/Executor_Framework/ExecutorDemo.java
@@ -1,7 +1,17 @@
 package com.Ayush.Multithreding.Executor_Framework;
 
+import java.util.concurrent.Executor;
+
 public class ExecutorDemo {
     public static void main(String[] args) {
+
+        Executor executor = command -> {
+            new Thread(command).start();
+        };
+        executor.execute(()-> System.out.println("Running...."));
+        executor.execute(()-> System.out.println("Running...."));
+        executor.execute(()-> System.out.println("Running...."));
+
 
     }
 }
