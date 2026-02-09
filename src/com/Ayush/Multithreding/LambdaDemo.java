3d9c8b456da0bf57bb813ce8cf2f4784ae42f589
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-09 17:42:35 +0530

    Add lambda-based thread creation example

--- src/com/Ayush/Multithreding/LambdaDemo.java
+++ src/com/Ayush/Multithreding/LambdaDemo.java
@@ -2,6 +2,9 @@ package com.Ayush.Multithreding;
 
 public class LambdaDemo {
     public static void main(String[] args) {
-
+    Thread t1 = new Thread(()->{
+        System.out.println("Hello!!");
+    });
+    t1.start();
     }
 }
