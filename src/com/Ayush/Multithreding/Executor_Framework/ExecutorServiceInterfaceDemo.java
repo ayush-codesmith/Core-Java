08474ddb630c28fbd0e1b3df2d84ebe827c44197
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-09 18:46:15 +0530

    Demonstrate task submission using ExecutorService

--- src/com/Ayush/Multithreding/Executor_Framework/ExecutorServiceInterfaceDemo.java
+++ src/com/Ayush/Multithreding/Executor_Framework/ExecutorServiceInterfaceDemo.java
@@ -7,6 +7,17 @@ import java.util.concurrent.Executors;
 public class ExecutorServiceInterfaceDemo {
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(3);
+        for (int i=0;i<10;i++){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            service.submit(()->{
+                System.out.println("Running...");
+            });
+        }
+        service.shutdown();
 
     }
 }
