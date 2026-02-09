44eafdffea69fd93067ba11a36983cda8326692e
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-09 20:02:49 +0530

    Future started

--- src/com/Ayush/Multithreding/Executor_Framework/ExecutorServiceInterfaceDemo.java
+++ src/com/Ayush/Multithreding/Executor_Framework/ExecutorServiceInterfaceDemo.java
@@ -3,6 +3,7 @@ package com.Ayush.Multithreding.Executor_Framework;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ExecutorServiceInterfaceDemo {
     private static Object TimeUnit;
@@ -12,11 +13,14 @@ public class ExecutorServiceInterfaceDemo {
         ExecutorService service = Executors.newFixedThreadPool(3);
         for (int i=0;i<10;i++){
 
-            service.submit(()->{
+
+          Future<?> future = service.submit(()->{
                 System.out.println("Running...");
             });
+            System.out.println(future.state());
 
         }
+
         service.shutdown();
 
         System.out.println(System.currentTimeMillis()-startTime);
