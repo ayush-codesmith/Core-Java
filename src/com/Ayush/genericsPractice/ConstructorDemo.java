86ace8a1b2017071464c90107e8d44ae2c9cdb94
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-11 15:19:58 +0530

    Implement generic constructor in ConstructorDemo with usage example
    Added a generic constructor <T> to demonstrate that constructors
    can define their own type parameters independent of the class.
    Included main method to test with String arguments.

--- src/com/Ayush/genericsPractice/ConstructorDemo.java
+++ src/com/Ayush/genericsPractice/ConstructorDemo.java
@@ -2,4 +2,12 @@ package com.Ayush.genericsPractice;
 
 public class ConstructorDemo {
 
+    public<T> ConstructorDemo(T a, T b){
+        System.out.println(a+" "+b);
+    }
+
+    public static void main(String[] args) {
+        ConstructorDemo obj = new ConstructorDemo("Ayush","Khandekar");
+
+    }
 }
