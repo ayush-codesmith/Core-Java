bd55d0ea21db0de6f2aafce44501c99b7ce4ce15
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-11 11:20:09 +0530

    Add ImlClassDemo main method to demonstrate class-level generics usage

--- src/com/Ayush/genericsPractice/ImlClassDemo.java
+++ src/com/Ayush/genericsPractice/ImlClassDemo.java
@@ -1,4 +1,10 @@
 package com.Ayush.genericsPractice;
 
 public class ImlClassDemo {
+    public static void main(String[] args) {
+        System.out.println("THis File Is Running.....");
+        ClassDemo<Integer> obj = new ClassDemo<>(2);
+        System.out.println(obj.getAge());
+
+    }
 }
