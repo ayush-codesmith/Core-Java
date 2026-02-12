29538926487b654909e7e7c4bebcf765867c8b36
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-12 23:33:44 +0530

    Add anonymous Runnable example to compare with lambda thread creation
    Replaced lambda-based Thread implementation with traditional
    anonymous inner class to demonstrate pre-Java 8 approach and
    highlight difference in verbosity and syntax.

--- src/com/Ayush/Java8/FunctionalInterface.java
+++ src/com/Ayush/Java8/FunctionalInterface.java
@@ -3,9 +3,18 @@ package com.Ayush.Java8;
 public class FunctionalInterface {
     public static void main(String[] args) {
     // with lambda expression
-        new Thread(()-> System.out.println("Thread started")).start();
+      //  new Thread(()-> System.out.println("Thread started")).start();
 
-        //
+        //if we don't use Lambda expression
+        //it will look like
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread started");
+            }
+        }).start();
+
+
 
     }
 }
