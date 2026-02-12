1452be07ed6e4347c7fdbde387181ee7d60a162e
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-12 23:19:54 +0530

    Introduce Java 8 functional interfaces (Predicate, Consumer, Supplier)
    Extended Lambda4 to demonstrate usage of built-in functional
    interfaces from java.util.function. Added examples of Predicate
    and Consumer (commented for reference) and implemented Supplier
    to generate random numbers using lambda expression.

--- src/com/Ayush/Java8/Lambda4.java
+++ src/com/Ayush/Java8/Lambda4.java
@@ -2,20 +2,41 @@ package com.Ayush.Java8;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
-public class Lambda4 {
+public class Lambda4  {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         List<String> list = new ArrayList<>();
         list.add("Ayush");
         list.add("Karan");
         list.add("Aramn");
         list.add("Prajwal");
 
-        list.forEach(names-> System.out.println(names));
-        System.out.println("Names start with 'A'");
-        list.stream()
-                .filter(n->n.startsWith("A"))
-                .map(n->n.toUpperCase())
-                .forEach(System.out::println);
+
+//        list.forEach(names-> System.out.println(names));
+//        System.out.println("Names start with 'A'");
+//        list.stream()
+//                .filter(n->n.startsWith("A"))
+//                .map(n->n.toUpperCase())
+//                .forEach(System.out::println);
+//        Predicate<String > result = n->n.startsWith("S");
+//        System.out.println(result.test("Sujal"));
+//
+//        Consumer<String > obj = n-> System.out.println("Hello "+n);
+//        obj.accept("Ayush");
+
+        Supplier<Integer> result1 = ()-> new Random().nextInt(100);
+
+        System.out.println(result1.get());
+
+
+
+
+
     }
 }
