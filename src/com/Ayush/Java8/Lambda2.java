48d74439ef00066acea09c61c71489287dfbb4d5
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-12 22:15:08 +0530

    Add lambda example with default method in functional interface
    Created Funinterface with one abstract method and a default method.
    Implemented it using a lambda expression in Lambda2 and demonstrated
    invoking both the lambda method and the interface default method.

--- src/com/Ayush/Java8/Lambda2.java
+++ src/com/Ayush/Java8/Lambda2.java
@@ -1,7 +1,15 @@
 package com.Ayush.Java8;
-
+interface Funinterface{
+    void  add(int x);
+    default void norFunc(){
+        System.out.println("Hello");
+    }
+}
 public class Lambda2 {
     public static void main(String[] args) {
-
+        Funinterface obj = (int x)-> System.out.println(2*x);
+        obj.add(2);
+        System.out.println(obj);
+        obj.norFunc();
     }
 }
