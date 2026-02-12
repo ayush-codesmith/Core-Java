e3e6a2f9a4a1d5623d7bed2368264d8f1a653914
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-12 23:30:30 +0530

    Demonstrate lambda usage with Thread and Runnable interface
    Added example of creating and starting a Thread using
    lambda expression instead of anonymous class to showcase
    practical use of functional interfaces in Java 8.

--- src/com/Ayush/Java8/FunctionalInterface.java
+++ src/com/Ayush/Java8/FunctionalInterface.java
@@ -2,6 +2,10 @@ package com.Ayush.Java8;
 
 public class FunctionalInterface {
     public static void main(String[] args) {
+    // with lambda expression
+        new Thread(()-> System.out.println("Thread started")).start();
+
+        //
 
     }
 }
