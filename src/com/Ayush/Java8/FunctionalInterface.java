d9e9c6f199e8bf461db652d70bac448e749e2d0c
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-12 23:40:59 +0530

    Add custom functional interface with @FunctionalInterface annotation
    Created CheckMethod interface to demonstrate functional interface
    rules and compile-time validation when adding multiple abstract methods.

--- src/com/Ayush/Java8/FunctionalInterface.java
+++ src/com/Ayush/Java8/FunctionalInterface.java
@@ -1,5 +1,10 @@
 package com.Ayush.Java8;
-
+@java.lang.FunctionalInterface
+interface CheckMethod{
+    void add(int a, int b);
+    // if we try to implementanother abstract methods so it will throw compile time errors...
+   // void mul(int a, int b);
+}
 public class FunctionalInterface {
     public static void main(String[] args) {
     // with lambda expression
