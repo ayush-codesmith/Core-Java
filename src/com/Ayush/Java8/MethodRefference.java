5b0a14d293f571526e5d87b38c0280790a8d62a1
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-13 00:49:51 +0530

    Add method reference example using static method
    Created MethodRefference class to demonstrate replacing
    lambda expression with method reference syntax in forEach.

---
+++ src/com/Ayush/Java8/MethodRefference.java
@@ -0,0 +1,14 @@
+package com.Ayush.Java8;
+
+import java.util.Arrays;
+
+public class MethodRefference {
+    public static void print(Integer num){
+        System.out.println(num);
+    }
+    public static void main(String[] args) {
+        Integer[] arr ={1,2,3,4,5,6,7,8};
+       // Arrays.stream(arr).forEach(n-> System.out.println(n));
+        Arrays.stream(arr).forEach(MethodRefference::print);
+    }
+}
