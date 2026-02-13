4703068a3d565f1d5baa9baf943f7230a7f57341
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-13 12:38:39 +0530

    Add instance method reference example for square calculation
    Introduced non-static squarN() method and used
    object reference with forEach to demonstrate
    instance method reference usage.

--- src/com/Ayush/Java8/MethodReferences1.java
+++ src/com/Ayush/Java8/MethodReferences1.java
@@ -7,11 +7,17 @@ public class MethodReferences1 {
     public static void squar(int n){
         System.out.println(n*n);
     }
+    //non static method
+    public void squarN(int n){
+        System.out.println(n*n);
+    }
     public static void main(String[] args) {
         // look using method references we complete all logic in 1 line.
+        MethodReferences1 methodReferences1 = new MethodReferences1();
         List<Integer> list = Arrays.asList(1,2,3,4,5);
         //list.forEach(MethodReferences1::squar); //
-        list.forEach(s->MethodReferences1.squar(s));
+       // list.forEach(s->MethodReferences1.squar(s));
+        list.forEach(methodReferences1::squarN);
 
     }
 }
