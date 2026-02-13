bee6e9dd9e0b47df9ae2cf3c728763cf86a0688b
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-13 14:14:18 +0530

    Add Bi-functional interfaces and operator examples
    Replaced single-argument functional interface examples
    with BiPredicate, BiFunction, BiConsumer, UnaryOperator,
    and BinaryOperator demonstrations.

--- src/com/Ayush/Java8/FunctionalInterfacePractice.java
+++ src/com/Ayush/Java8/FunctionalInterfacePractice.java
@@ -1,24 +1,36 @@
 package com.Ayush.Java8;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class FunctionalInterfacePractice {
     public static void main(String[] args) {
-        // predict
-        Predicate<Integer> predicate = x-> x%2==0;
-        System.out.println(predicate.test(2));
+//        // predict
+//        Predicate<Integer> predicate = x-> x%2==0;
+//        System.out.println(predicate.test(2));
+//
+//        Function<Integer, Integer> function = x->2*x;
+//        System.out.println(function.apply(2));
+//
+//        Consumer<String> consumer = (s) -> System.out.println("Ayush "+s);
+//        consumer.accept("Khandekar");
+//
+//        Supplier<String> stringSupplier = ()-> "Ayush";
+//        System.out.println(stringSupplier.get());
+    // this only for single attribute if we want double attribute so use below
 
-        Function<Integer, Integer> function = x->2*x;
-        System.out.println(function.apply(2));
+        BiPredicate<Integer,Integer> predicate1= (x,y)->x*y%2==0;
+        System.out.println(predicate1.test(2,2));
+        BiFunction<Integer,Integer,Integer> biFunction = (x,y)->x*y*3;
+        System.out.println(biFunction.apply(2,2));
+        BiConsumer<Integer,Integer> biConsumer = (a,b)-> System.out.println(a+" "+b);
+        biConsumer.accept(2,2);
 
-        Consumer<String> consumer = (s) -> System.out.println("Ayush "+s);
-        consumer.accept("Khandekar");
+        // there are also some Interfaces
+        UnaryOperator<Integer> unaryOperator = x -> x*3;
+        System.out.println(unaryOperator.apply(3));
+        BinaryOperator<Integer> binaryOperator = (x,y)->x*y;
+        System.out.println(binaryOperator.apply(2,2));
 
-        Supplier<String> stringSupplier = ()-> "Ayush";
-        System.out.println(stringSupplier.get());
 
 
     }
