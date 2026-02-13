dce34caefc5380e3643d04cad7c4ce35b7e80d33
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-13 13:03:52 +0530

    Add constructor reference example using Supplier
    Introduced class constructor and demonstrated
    object creation using Supplier with
    MethodReferences2::new constructor reference.

--- src/com/Ayush/Java8/MethodReferences2.java
+++ src/com/Ayush/Java8/MethodReferences2.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Locale;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class MethodReferences2 {
+    MethodReferences2(){
+        System.out.println("We create Object of this class using Functional Interface !!");
+    }
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         list.add("Ayush");
@@ -21,6 +26,9 @@ public class MethodReferences2 {
 
        //}
 
-        list.forEach(n-> System.out.println(n.toUpperCase(Locale.ROOT)));
+        //list.forEach(n-> System.out.println(n.toUpperCase(Locale.ROOT)));
+        // below we create object of this class using Functional Interface
+        Supplier<MethodReferences2> obj = MethodReferences2::new;
+        obj.get();
     }
 }
