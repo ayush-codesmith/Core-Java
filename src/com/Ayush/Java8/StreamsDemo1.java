e222322275c31711c3e30446fff1f9749a4c5cd9
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-13 18:36:38 +0530

    Add StreamsDemo1 with basic stream creation example
    Demonstrates creating a stream from a list and
    iterating using stream().forEach().

---
+++ src/com/Ayush/Java8/StreamsDemo1.java
@@ -0,0 +1,21 @@
+package com.Ayush.Java8;
+
+import java.util.ArrayList;
+
+public class StreamsDemo1 {
+    public static void main(String[] args) {
+        //ways to create or use Streams
+        //1 way using list
+        ArrayList<Object> objects = new ArrayList<>();
+        objects.add(1);
+        objects.add("Ayush");
+        objects.add(3.3);
+        //objects.forEach(System.out::println);
+       // objects.stream().forEach(System.out::println);
+
+//        for (Object a :objects){
+//            System.out.println(a);
+        //}
+
+    }
+}
