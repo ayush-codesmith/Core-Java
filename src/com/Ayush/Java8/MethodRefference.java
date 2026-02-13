37893faa968f849d06049843a43a0feee6af21e2
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-13 12:17:31 +0530

    Replace method reference with equivalent lambda expression
    Updated forEach call to use lambda instead of
    MethodRefference::print to compare both approaches.

--- src/com/Ayush/Java8/MethodRefference.java
+++ src/com/Ayush/Java8/MethodRefference.java
@@ -9,6 +9,8 @@ public class MethodRefference {
     public static void main(String[] args) {
         Integer[] arr ={1,2,3,4,5,6,7,8};
        // Arrays.stream(arr).forEach(n-> System.out.println(n));
-        Arrays.stream(arr).forEach(MethodRefference::print);
+        //Arrays.stream(arr).forEach(MethodRefference::print);
+       Arrays.stream(arr).forEach(s->MethodRefference.print(s));
+
     }
 }
