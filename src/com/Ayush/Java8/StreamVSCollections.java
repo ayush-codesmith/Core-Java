f31a243b3af48b17ed4c8981e9ce021a67bbf915
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-14 13:55:11 +0530

    Update StreamVSCollections with traditional collection sorting example
    Added ArrayList initialization and demonstrated sorting
    using Comparator and Collections.sort() to contrast
    with Stream-based approaches.

--- src/com/Ayush/Java8/StreamVSCollections.java
+++ src/com/Ayush/Java8/StreamVSCollections.java
@@ -1,7 +1,19 @@
 package com.Ayush.Java8;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class StreamVSCollections {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Kunal");
+        list.add("Ayush");
+        list.add("Balu");
 
+        Comparator<String > com = (O1 , O2)->O1.compareTo(O2);
+        Collections.sort(list,com);
+        System.out.println(list);
     }
 }
