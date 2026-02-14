0dcb1c2f017d71dd9b97b759789788ec554dc0b2
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-14 14:31:33 +0530

    Update OptionalDemo1 to demonstrate NullPointerException scenario
    Added example showing default null values in an array
    and triggering a NullPointerException to highlight
    the need for Optional in null-safe handling.

--- src/com/Ayush/Java8/OptionalDemo1.java
+++ src/com/Ayush/Java8/OptionalDemo1.java
@@ -2,6 +2,15 @@ package com.Ayush.Java8;
 
 public class OptionalDemo1 {
     public static void main(String[] args) {
+    String [] word = new String[10];
+    // by default it has 10 null values
+        String wordprint = word[5].toUpperCase();
+        System.out.println(wordprint);
+        //Exception in thread "main" java.lang.NullPointerException: Cannot invoke "String.toUpperCase()" because "word[5]" is null
+        //	at com.Ayush.Java8.OptionalDemo1.main(OptionalDemo1.java:7)
+
+        //it will throw run time Error NPE
+
 
     }
 }
