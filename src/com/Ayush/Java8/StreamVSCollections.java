0af507a89f069e545e80e85b7d90af34c96e2415
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-14 13:57:02 +0530

    Enhance StreamVSCollections with Java 8 stream sorting example
    Added stream().sorted().forEach() demonstration to
    compare Stream-based sorting with traditional
    Collections.sort() approach.

--- src/com/Ayush/Java8/StreamVSCollections.java
+++ src/com/Ayush/Java8/StreamVSCollections.java
@@ -15,5 +15,12 @@ public class StreamVSCollections {
         Comparator<String > com = (O1 , O2)->O1.compareTo(O2);
         Collections.sort(list,com);
         System.out.println(list);
+
+        // now we use java 8
+        List<String> list1 =new ArrayList<>();
+        list1.add("Kunal");
+        list1.add("Ayush");
+        list1.add("Balu");
+        list1.stream().sorted().forEach(System.out::println);
     }
 }
