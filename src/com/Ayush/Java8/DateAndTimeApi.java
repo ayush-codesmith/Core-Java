2b69eb08e9af9a2c8281e2b10bdeb354bd76cf32
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-14 19:50:33 +0530

    Enhance DateAndTimeApi with Instant example
    Added usage of Instant.now() to demonstrate
    timestamp generation and included commented
    examples of LocalDate, LocalTime, LocalDateTime,
    and ZonedDateTime.

--- src/com/Ayush/Java8/DateAndTimeApi.java
+++ src/com/Ayush/Java8/DateAndTimeApi.java
@@ -1,7 +1,22 @@
 package com.Ayush.Java8;
 
+import java.time.*;
+
 public class DateAndTimeApi {
     public static void main(String[] args) {
+//        LocalDate now = LocalDate.now();
+//        System.out.println(now);
+//
+//        LocalTime now1 = LocalTime.of(2,10);
+//        System.out.println(now1);
+
+//        LocalDateTime now = LocalDateTime.now();
+//        System.out.println(now);
+
+//        ZonedDateTime nagpur = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+//        System.out.println(nagpur.getMonth()+":"+nagpur.getMinute());
+        Instant now = Instant.now();
+        System.out.println(now);
 
     }
 }
