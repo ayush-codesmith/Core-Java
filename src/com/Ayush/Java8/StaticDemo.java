afebef18e2b10c3a65efa2dd455a9c269a162ff4
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-14 19:03:40 +0530

    Update StaticDemo to demonstrate interface static method usage
    Added interface AA with a static method and
    demonstrated invoking it using InterfaceName.methodName()
    inside main().

--- src/com/Ayush/Java8/StaticDemo.java
+++ src/com/Ayush/Java8/StaticDemo.java
@@ -1,7 +1,12 @@
 package com.Ayush.Java8;
-
+interface AA{
+    static void show(){
+        System.out.println("!!!!!!!");
+    }
+}
 public class StaticDemo {
     public static void main(String[] args) {
+        AA.show();
 
     }
 }
