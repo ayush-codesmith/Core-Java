c40fef5b00240e0c263fe83f7bf20667e58968c0
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-14 23:45:47 +0530

    Implement JDBC Connections example for MySQL database access
    Added database connection setup using DriverManager,
    loaded MySQL driver, executed SELECT query on student
    table, and processed ResultSet to display retrieved data.

--- src/com/Ayush/JDBC/Connections.java
+++ src/com/Ayush/JDBC/Connections.java
@@ -1,7 +1,39 @@
 package com.Ayush.JDBC;
 
+import java.sql.*;
+
 public class Connections {
+    private static final String url ="jdbc:mysql://localhost:3306/mydb";
+    private static final String username = "root";
+    private static final String password = "Java@80100";
     public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            Connection connections = DriverManager.getConnection(url,username,password);
+            Statement statement = connections.createStatement();
+            String query = " SELECT * FROM student";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                int Id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                double marks = resultSet.getDouble("marks");
+                System.out.println(Id);
+                System.out.println(name);
+                System.out.println(age);
+                System.out.println(marks);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
