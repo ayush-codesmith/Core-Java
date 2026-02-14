8eab6cc7662eb06a36b13571ab033931face918e
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-14 18:51:41 +0530

    Update DefaultDemo to demonstrate multiple interface default methods
    Added interfaces A, B, and C with default methods and
    implemented them in DefaultDemo. Demonstrated resolving
    default methods using InterfaceName.super.method()
    and overriding abstract method add().

--- src/com/Ayush/Java8/DefaultDemo.java
+++ src/com/Ayush/Java8/DefaultDemo.java
@@ -1,7 +1,36 @@
 package com.Ayush.Java8;
+interface A{
+    public void add(int a, int b);
 
-public class DefaultDemo {
+    default void show(){
+        System.out.println("This is default method");
+    }
+}
+interface B{
+    default void print(){
+        System.out.println("Hello this is 2 default method");
+    }
+}
+interface C{
+    default void printc(){
+        System.out.println("Hello this is 3 default method");
+    }
+}
+public class DefaultDemo implements A ,B,C{
+
+    @Override
+    public void add(int a, int b) {
+        System.out.println(a+b);
+    }
+    public void showM(){
+        System.out.println("I'm Default class method which is calling all method you can see below");
+        A.super.show();
+        B.super.print();
+        C.super.printc();
+    }
     public static void main(String[] args) {
-
+    DefaultDemo obj = new DefaultDemo();
+    obj.add(2,2);
+    obj.showM();
     }
 }
