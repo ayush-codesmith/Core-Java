f23e5f7e4862a04897c50b95ab060be15c110a9b
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-17 21:31:09 +0530

    Add Practice1 JDBC example for MySQL connection and query execution
    Implemented database connection using DriverManager,
    executed SELECT query on users table, and retrieved
    results using ResultSet to display id, name, and surname.

---
+++ src/com/Ayush/JDBC/Practice1.java
@@ -0,0 +1,33 @@
+package com.Ayush.JDBC;
+
+import java.sql.*;
+
+public class Practice1 {
+    private static final String  url="jdbc:mysql://127.0.0.1:3306/practice1";
+    private static final String  user="root";
+    private static final String  password="Java@80100";
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection connection = DriverManager.getConnection(url,user,password);
+            Statement statement = connection.createStatement();
+            String query ="SELECT * FROM users";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+                System.out.println("ID :" +id);
+                System.out.println("Name :"+name);
+                System.out.println("Surname :"+surname);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
