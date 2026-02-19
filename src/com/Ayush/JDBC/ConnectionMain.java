a62d033a1bf59b6ba923177db7dd67422ef1fa9e
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-19 11:11:24 +0530

    fix(jdbc): update database URL to specify schema name

--- src/com/Ayush/JDBC/ConnectionMain.java
+++ src/com/Ayush/JDBC/ConnectionMain.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionMain {
-    private static final String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/port1";
     private static final String userName = "root";
     private static final String password = "Java@80100";
     public static Connection getConnection(){
