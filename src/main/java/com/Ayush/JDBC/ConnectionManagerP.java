fa20cd45afdb5619ea6bac88f2f51bb062ee6037
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-19 18:05:45 +0530

    feat(db): implement standalone connection pooling in ConnectionManagerP for practice
    - Configure HikariCP with JDBC URL and credentials
    - Initialize static HikariDataSource
    - Provide getConnection() helper method
    - Add datasource close() utility

--- src/main/java/com/Ayush/JDBC/ConnectionManagerP.java
+++ src/main/java/com/Ayush/JDBC/ConnectionManagerP.java
@@ -1,5 +1,35 @@
 package com.Ayush.JDBC;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ConnectionManagerP {
+private static final HikariDataSource dataSource;
+
+static {
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/?");
+    config.setUsername("root");
+    config.setPassword("Java@80100");
+    config.setMaximumPoolSize(10);
+
+    dataSource = new HikariDataSource(config);
+}
+
+public static Connection getConnection(){
+    try {
+        return dataSource.getConnection();
+    }catch (SQLException e){
+        throw  new RuntimeException("Failed to get DB commection "+e);
+    }
+}
+public static void close(){
+    if (dataSource!=null){
+        dataSource.close();
+    }
+}
 
 }
