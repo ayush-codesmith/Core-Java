030b59894bff20074a9fe1cdace2f7118f65def3
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-02-19 18:44:50 +0530

    feat(db): enhance ImpleP to execute query using pooled connection
    - Fetch data from users table
    - Use PreparedStatement and ResultSet
    - Implement try-with-resources for safe handling
    - Close datasource after execution

--- src/main/java/com/Ayush/JDBC/ImpleP.java
+++ src/main/java/com/Ayush/JDBC/ImpleP.java
@@ -1,7 +1,27 @@
 package com.Ayush.JDBC;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ImpleP {
     public static void main(String[] args) {
+        try (Connection connection = ConnectionManagerP.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users");
+             ResultSet resultSet = preparedStatement.executeQuery()) {
 
+            while (resultSet.next()){
+
+                System.out.println(resultSet.getString("name")+" "+resultSet.getString("surname"));
+            }
+
+
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        ConnectionManagerP.close();
     }
 }
