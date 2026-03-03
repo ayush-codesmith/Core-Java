3a7e5a2c1690d53b2ba579d4aa71483a921af66a
Raju Khan <rajukhannn151@gmail.com>
2026-03-03 16:22:31 +0530

    First File done

---
+++ src/main/java/git/Ayush/File1.java
@@ -0,0 +1,17 @@
+package git.Ayush;
+
+public class File1 {
+    public static void main(String[] args) {
+        //Hello my name is ayush khandekar
+
+        for (int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+
+        //second looop
+        for (int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+
+}
