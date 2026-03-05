f4920366a0837b5c4db2a18806f450f91ae861ac
Raju Khan <rajukhannn151@gmail.com>
2026-03-05 22:22:17 +0530

    Added DSA practice files and cleaned embedded repo

---
+++ src/main/java/com/Ayush/DSA/Basics/Test.java
@@ -0,0 +1,12 @@
+package com.Ayush.DSA.Basics;
+
+public class Test {
+    public static void main(String[] args) {
+        //Given a number n, check whether it is even or odd. Return true for even and false for odd.
+
+        int n = 15;
+        String s = (n % 2 == 0) ? "True" : "False";
+        System.out.println(s);
+
+    }
+}
---
+++ src/main/java/com/Ayush/DSA/TEST.java
@@ -0,0 +1,5 @@
+package com.Ayush.DSA;
+
+public class TEST {
+
+}
