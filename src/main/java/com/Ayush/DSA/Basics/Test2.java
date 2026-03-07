ae4a68d7f9b0f18cb9f4fb00f04e5a1d3eb2ce9d
Raju Khan <rajukhannn151@gmail.com>
2026-03-07 18:50:35 +0530

    Day 6

--- src/main/java/com/Ayush/DSA/Basics/Test2.java
+++ src/main/java/com/Ayush/DSA/Basics/Test2.java
@@ -18,9 +18,82 @@ public class Test2 {
         }
         return sum;
     }
-    public static void main(String[] args) {
-        //table(2);
-        System.out.println(naturalN(3)); // so the lop run on n we don't have fix Iteration O(n) // 6
 
+    public static int naturalNO(int n){
+        return n*(n+1)/2 ;// herer only 3 steps no matter what is input n
+    }
+
+    //Reverse a number
+    public static void reverseN(int n){
+        int reverse = 0;
+        while (n!=0){
+            reverse = reverse*10 + (n%10);
+            n=n/10;
+
+        }
+        System.out.println(reverse);
+    }
+
+//    Given a positive integer n, we have to find the sum of squares of first n natural numbers.
+
+    public static void squareN(int n){
+        // n = 2 so now from 1^2 + 2 ^2 = 5
+        int square =0;
+        for (int i=1;i<=n;i++){
+            square+=i*i;
+        }
+        System.out.println(square);
+
+    }
+
+
+    public void sawpN(int a, int b){
+        // input a=2 , b=9
+        int temp = a; // 2
+        a=b;//9
+        b=temp; //2
+        System.out.println(a+ " " +b);
+    }
+
+    public static int squarNO(int n){
+        return n*(n+1)*(2*n+1)/6;
+    }
+
+    public static void main(String[] args) {
+
+
+
+   Test2 test2 = new Test2();
+   test2.sawpN(2,9); // steps are constandt so TC O(1) SC is O(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        //table(2);
+//       // System.out.println(naturalN(3)); // so the lop run on n we don't have fix Iteration O(n) // 6
+//       // System.out.println(naturalNO(1000)); // now only 3 steps happen not 1000 so TC is O(1) and SC O(1)
+////        squareN(2)
+//        System.out.println(squarNO(2)); // now we have constant steps so TC is O(1) and SC is O(1)
     }
 }
